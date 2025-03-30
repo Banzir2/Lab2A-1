fd0eeb3a556a00788eeaef1ab554e2b07c171868
Banzir2 <eitan.vichik.46@gmail.com>
2025-03-30 16:21:29 +0300

    Important changes

--- partA.m
+++ partA.m
@@ -9,14 +9,15 @@ x = [559.2 552.7 545.1 537.7 531.6 524.3 517.3 509.5 503.7 496.6 488.9, ...
     481.6 474.1 467.2 460 452.5 446.3 439.4 431.6].' / 1000;
 mass_error = [0.01, 0.02, 0.02, 0.03, 0.02, 0.03, 0.03, 0.04, 0.04, ...
     0.05, 0.02, 0.03, 0.03, 0.04, 0.03, 0.04, 0.04, 0.05, 0.05].' / sqrt(12) / 1000;
-x_error = ones(19, 1) * (0.3 / 1000);
+x_error = ones(19, 1) * ((0.3 / sqrt(200)) / 1000);
 
 hold on;
 
 zero_height = ones(19, 1) * 559.2 / 1000;
 x = zero_height - x;
 g = 9.80665;
-errorbar(x, mass * g, x_error, x_error, mass_error * g, mass_error * g, '.');
+e = errorbar(x, mass * g, x_error, x_error, mass_error * g, mass_error * g, '.');
+e.CapSize = 0;
 f = fit(x, mass * g, 'poly1');
 
 k = 6.938;
