6a3b38785cb3558b6b48ceab076f6d8dbde346f2
Banzir2 <eitan.vichik.46@gmail.com>
2025-03-30 16:09:03 +0300

    C

--- partA.m
+++ partA.m
@@ -8,8 +8,8 @@ mass = mass + platform;
 x = [559.2 552.7 545.1 537.7 531.6 524.3 517.3 509.5 503.7 496.6 488.9, ...
     481.6 474.1 467.2 460 452.5 446.3 439.4 431.6].' / 1000;
 mass_error = [0.01, 0.02, 0.02, 0.03, 0.02, 0.03, 0.03, 0.04, 0.04, ...
-    0.05, 0.02, 0.03, 0.03, 0.04, 0.03, 0.04, 0.04, 0.05, 0.05].' / 1000;
-x_error = ones(19, 1) * (0.02 / 1000);
+    0.05, 0.02, 0.03, 0.03, 0.04, 0.03, 0.04, 0.04, 0.05, 0.05].' / sqrt(12) / 1000;
+x_error = ones(19, 1) * (0.3 / 1000);
 
 hold on;
 
@@ -20,5 +20,9 @@ errorbar(x, mass * g, x_error, x_error, mass_error * g, mass_error * g, '.');
 f = fit(x, mass * g, 'poly1');
 
 k = 6.938;
+theo_force = f(x);
+rsquare = (mass * g - theo_force).^2;
+xhisquare = sum(rsquare ./ ((k*x_error).^2 + (mass_error * g).^2)) / 17;
+
 omega0 = sqrt((ones(19, 1)*k) ./ mass);
 figure; scatter(mass, omega0);
